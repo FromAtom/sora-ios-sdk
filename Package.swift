5a0d11861db3fac07a6e74566bb080fd3a4e1166
FromAtom <fromatom@gmail.com>
2021-11-06 18:30:38 +0900

    コメントを削除

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
     platforms: [.iOS(.v12)],
     products: [
         .library(name: "Sora", targets: ["Sora"])
-        //.library(name: "WebRTC", targets: ["WebRTC"]),
     ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.4")),
